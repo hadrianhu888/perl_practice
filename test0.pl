3b0f1b387b5bbbeb0684f158f9c8e03b9b308537
hadrianhu888 <hadrian.hu@gmail.com>
2022-01-19 19:30:57 -0500

    first commit

---
+++ test0.pl
@@ -0,0 +1,45 @@
+=start comment 
+this is a perl comment 
+=cut 
+
+print "Hello World"; 
+
+$string = "\n\nThis is a test string"; 
+
+print $string; 
+$a = 1; 
+$b = 2; 
+$c = $a - $b; 
+$d= $a + $b; 
+$e = $a * $b; 
+$f = $a / $b; 
+
+=start comment 
+addition, subtraction, division, multiplication operations 
+=cut 
+
+print "\n\n"; 
+
+print "The result is "; 
+print $c; 
+
+print "\n\n"; 
+
+print "The result is "; 
+print $d; 
+
+print "\n\n"; 
+
+print "The result is "; 
+print $e; 
+
+print "\n\n"; 
+
+print "The result is "; 
+print $f; 
+
+print "\n\n"; 
+
+=start 
+End of the program 
+=end 
