95d2add18bb0ffc04facaeb5bf6bb184b26804b6
hadrianhu888 <hadrian.hu@gmail.com>
2022-02-06 14:46:19 -0500

    updated referencing examples

--- 1_perl_extract_vars.pl
+++ 1_perl_extract_vars.pl
@@ -9,9 +9,29 @@ open(my $fh, "<", $file) or die "Unable to open < $file: $!";
 my @v_vars = <$fh>;
 chomp(@v_vars);
 print @v_vars;
+my @var_array = (); 
+
 #prints each line separately
 foreach (@v_vars)
 {
     print("$_\n");
+    push(@var_array, $_);
 }
 
+foreach my $i (@var_array)
+{
+    print($i ."\n");
+}
+
+my $var_ref = \@var_array; 
+
+print $var_ref; 
+
+print("\n\n");
+
+print "ELements are @$var_ref\n\n";
+
+foreach my $i (@$var_ref)
+{
+    print {@$var_ref}[$i]* ."\n\n";
+}
--- perl_references.pl
+++ perl_references.pl
@@ -28,3 +28,32 @@ $r = \%var;
 # Print values available at the location stored in $r.
 print "Value of %var is : ", %$r, "\n";
 
+$var = 10;
+$r = \$var;
+print "Reference type in r : ", ref($r), "\n";
+
+@var = (1, 2, 3);
+$r = \@var;
+print "Reference type in r : ", ref($r), "\n";
+
+%var = ('key1' => 10, 'key2' => 20);
+$r = \%var;
+print "Reference type in r : ", ref($r), "\n";
+
+#!/usr/bin/perl
+
+# Function definition
+sub PrintHash {
+   my (%hash) = @_;
+   
+   foreach my $item (%hash) {
+      print "Item : $item\n";
+   }
+}
+my %hash = ('name' => 'Tom', 'age' => 19);
+
+# Create a reference to above function.
+my $cref = \&PrintHash;
+
+# Function call using reference.
+&$cref(%hash);
